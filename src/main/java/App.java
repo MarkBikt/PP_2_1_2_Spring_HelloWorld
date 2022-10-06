6fc9a68abe779f62b745f0afc314a69c1fdf57ca
Mark <mark.biktiryakov@rambler.ru>
2022-10-06 18:23:43 +0300

    2_1_2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,28 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getMessage());
+
+        System.out.println(bean == bean2);
+        System.out.println(bean3 == bean4);
     }
 }
+//1. Прочитайте про виды бинов.
+//2. Создайте POJO-класс Cat.
+//3. В классе AppConfig, по аналогии, создайте бин с именем “cat”.
+//4. Настройте этот бин так, чтобы он создавался новым при каждом запросе.
+//5. В классе App, по аналогии, вызовите еще раз бин HelloWorld,
+// затем 2 раза вызовите бин cat. Сравните 2 пары бинов по ссылке и выведите результат в консоль.
+// Для пары HelloWorld должно вернуться true, для cat - false. Так вы можете увидеть результат того, как работает наш контейнер.
+//6. Раскомментируйте тестовый класс и проверьте своё решение.
